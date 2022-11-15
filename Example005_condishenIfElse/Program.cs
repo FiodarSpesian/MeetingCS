cda80a9f3e94562476d2584d6c43c13755408010
Fiodar <fiodarspesian@mail.ru>
2022-11-15 07:41:17 +0300

    Add Example 005 if-else

---
+++ Example005_condishenIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
